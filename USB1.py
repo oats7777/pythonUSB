91425e0e6b026f07e76c5461a9b7e778fe8e39e9
minkyu <hanmin84200@gamil.com>
2018-07-27 20:22:33 +0900

    2018-07-27 PM 8:21
    예외처리

--- USB1.py
+++ USB1.py
@@ -10,19 +10,22 @@ USB에 있는 내용을 원하는 폴더로 옳기는 프로그램
 a=glob.glob("D:\\*")
 b=a[:]
 i=0
-while i < len(a):
-    a[i]=a[i].replace("D:\\","C:\\USBCOPY\\")
-    if a[i] == 'C:\\USBCOPY\\System Volume Information':
-        del a[i]
-        del b[i]
-        continue
-    i=i+1
-print (a)
-i=0
-while i < len(a):
-    try:
-        shutil.copy(b[i],a[i])
-    except:
-        shutil.copytree(b[i],a[i])
-    finally:
+if a:
+    while i < len(a):
+        a[i]=a[i].replace("D:\\","C:\\USBCOPY\\")
+        if a[i] == 'C:\\USBCOPY\\System Volume Information':
+            del a[i]
+            del b[i]
+            continue
         i=i+1
+    print (a)
+    i=0
+    while i < len(a):
+        try:
+            shutil.copy(b[i],a[i])
+        except:
+            shutil.copytree(b[i],a[i])
+        finally:
+            i=i+1
+else:
+    print ("usb가 인식되어 있지 않습니다.")
